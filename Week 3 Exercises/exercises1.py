a0a5a646686a4d8ba9b03461f53f14e980457deb
RUOCHEN-LYU <156784721+RUOCHEN-LYU@users.noreply.github.com>
2024-01-26 21:02:56 +0000

    Add files via upload

---
+++ Week 3 Exercises/exercises1.py
@@ -0,0 +1,13 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+    "brand": "Benz",
+    "model": "x5",
+    "year": 1980
+}
+print(thisdict["brand"])
