d3ccdd8a2e8dcac857b507b2210bdb98a0f59149
RUOCHEN-LYU <156784721+RUOCHEN-LYU@users.noreply.github.com>
2024-01-27 17:02:43 +0000

    Update exercises3.py

--- Week 3 Exercises/exercises3.py
+++ Week 3 Exercises/exercises3.py
@@ -4,3 +4,15 @@ def my_function(fname):
 my_function("Bill")
 my_function("steve")
 my_function("Elon")
+
+def my_function(x):
+    return 5 * x
+    
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+def my_function(child3, child2, child1):
+    print("The youngest child is " + child3)
+
+my_function(child1 = "Phoebe", child2= "Jennifer", child3= "Rory")
