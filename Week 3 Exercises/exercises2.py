b5a35e70736cc4b2e9c104633acac32413229d6f
RUOCHEN-LYU <156784721+RUOCHEN-LYU@users.noreply.github.com>
2024-01-27 16:12:28 +0000

    Add files via upload

---
+++ Week 3 Exercises/exercises2.py
@@ -0,0 +1,15 @@
+a = 66
+b = 66
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+c = 200
+d = 67
+if d >c:
+    print("d is greater than c")
+elif d == c:
+    print("d and c are equal")
+else:
+    print("c is greater than d")
